0b227a11e31870fabc10adc9609e2946bdc34ea5
David Nisson <d.m.nisson@gmail.com>
2019-07-13 10:33:45 -0700

    added view models

---
+++ src/main/java/labvision/viewmodels/Dashboard.java
@@ -0,0 +1,134 @@
+package labvision.viewmodels;
+
+import labvision.entities.Experiment;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import labvision.entities.Course;
+
+/**
+ * Represents a set of objects to be displayed in the student dashboard
+ * @author davidnisson
+ *
+ */
+public class Dashboard {
+	// experiments which are currently available for the student to enter values into
+	private final HashMap<Course, Experiment> currentExperiments = new HashMap<>();
+	
+	// most recent experiments which the student has completed
+	private final ArrayList<Experiment> recentExperiments = new ArrayList<>();
+	
+	// current and most recent courses
+	private final ArrayList<Course> recentCourses = new ArrayList<>();
+	
+	// maximum number of recent experiments to list
+	private int maxRecentExperiments;
+	
+	// maximum number of recent courses to list
+	private int maxRecentCourses;
+
+	/**
+	 * Get a map view of courses to current experiments
+	 * @return the map of courses to current experiments
+	 */
+	public Map<Course, Experiment> getCurrentExperiments() {
+		return currentExperiments;
+	}
+
+	/**
+	 * Set the map of courses to current experiments from a given map
+	 * @param currentExperiments the map of courses to current experiments
+	 */
+	public void setCurrentExperiments(Map<? extends Course, ? extends Experiment> currentExperiments) {
+		this.currentExperiments.putAll(currentExperiments);
+	}
+	
+	/**
+	 * Get the current experiment for a course
+	 * @param course the course
+	 * @return the current experiment
+	 */
+	public Experiment getCurrentExperiment(Course course) {
+		return currentExperiments.get(course);
+	}
+	
+	/**
+	 * Set the current experiment for a course
+	 * @param course the course
+	 * @param experiment the experiment
+	 */
+	public void setCurrentExperiment(Course course, Experiment experiment) {
+		this.currentExperiments.put(course, experiment);
+	}
+
+	/**
+	 * Get a list of the most recent experiments
+	 * @return the list of most recent experiments, with the set maximum size
+	 */
+	public List<Experiment> getRecentExperiments() {
+		return recentExperiments.subList(0, 
+				Math.min(maxRecentExperiments, recentExperiments.size()));
+	}
+
+	/**
+	 * Set the list of most recent experiments from the given list
+	 * @param recentExperiments the most recent experiments
+	 */
+	public void setRecentExperiments(List<? extends Experiment> recentExperiments) {
+		this.recentExperiments.clear();
+		this.recentExperiments.addAll(recentExperiments);
+	}
+
+	/**
+	 * Add a new experiment to the list of most recent ones
+	 * @param experiment the experiment to add
+	 */
+	public void addRecentExperiment(Experiment experiment) {
+		this.recentExperiments.add(0, experiment);
+	}
+	
+	/**
+	 * Remove old experiments that are stored in the list but are not displayed
+	 * because of the maximum size.
+	 */
+	public void pruneOldExperiments() {
+		this.recentExperiments.retainAll(this.getRecentExperiments());
+	}
+	
+	/**
+	 * Get a list of the most recent courses
+	 * @return the list of most recent courses
+	 */
+	public List<Course> getRecentCourses() {
+		return recentCourses.subList(0, 
+				Math.min(maxRecentCourses, recentCourses.size()));
+	}
+	
+	/**
+	 * Set the list of most recent courses from the given list
+	 * @param recentCourses the most recent courses
+	 */
+	public void setRecentCourses(List<? extends Course> recentCourses) {
+		this.recentCourses.clear();
+		this.recentCourses.addAll(recentCourses);
+	}
+	
+	/**
+	 * Add a new course to the list of most recent ones
+	 * @param course the course to add
+	 */
+	public void addRecentCourse(Course course) {
+		this.recentCourses.add(0, course);
+	}
+	
+	/**
+	 * Remove old courses that are stored in the list but are not displayed
+	 * because of the maximum size.
+	 */
+	public void pruneOldCourses() {
+		this.recentCourses.retainAll(this.getRecentCourses());
+	}
+}
---
+++ src/main/java/labvision/viewmodels/Pagination.java
@@ -0,0 +1,141 @@
+package labvision.viewmodels;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.TreeSet;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
+
+/**
+ * Model for pagination of a list of entities
+ * @author davidnisson
+ *
+ * @param <E> the entity being paginated
+ */
+public class Pagination<E> {
+	/**
+	 * Number of entities per page
+	 */
+	private int entitiesPerPage;
+	
+	/**
+	 * List of entities being paginated
+	 */
+	private List<E> entityList;
+	
+	/**
+	 * Number of pages
+	 */
+	private int numberOfPages;
+	
+	/**
+	 * The maximum number of consecutive pages around the current page
+	 * to which to display links
+	 */
+	private int numberOfConsecutivePagesToLink;
+	
+	/**
+	 * Indices of first element of each page
+	 */
+	private HashMap<Integer, Integer> firstElementIndices;
+
+	public int getEntitiesPerPage() {
+		return entitiesPerPage;
+	}
+
+	public void setEntitiesPerPage(int entitiesPerPage) {
+		this.entitiesPerPage = entitiesPerPage;
+	}
+
+	public List<E> getEntityList() {
+		return entityList;
+	}
+
+	public void setEntityList(List<E> entityList) {
+		this.entityList = entityList;
+		computePagination();
+	}
+
+	/**
+	 * Compute the number of pages and first element indices of each page
+	 */
+	public void computePagination() {
+		// compute number of pages
+		numberOfPages = entityList.size() / entitiesPerPage;
+		if (entityList.size() % entitiesPerPage > 0) {
+			numberOfPages++;
+		}
+		
+		// repopulate first element index map
+		firstElementIndices.clear();
+		IntStream.range(1, numberOfPages + 1)
+		.forEach(i -> {
+			firstElementIndices.put(i, (i - 1) * entitiesPerPage);
+		});
+	}
+
+	public int getNumberOfPages() {
+		return numberOfPages;
+	}
+
+	/**
+	 * Get the page numbers to which to link
+	 * @param pageNumber the page number
+	 * @return the page numbers
+	 */
+	public List<Integer> getPagesToLink(int pageNumber) {
+		ArrayList<Integer> pageNumbersToLink = new ArrayList<>();
+		
+		// add first page
+		pageNumbersToLink.add(1);
+		
+		// add range of pages around the current page
+		int lowBound = Math.max(2, pageNumber - (numberOfConsecutivePagesToLink / 2));
+		int highBound = Math.min(numberOfPages, 
+				pageNumber + ceil2(numberOfConsecutivePagesToLink));
+		pageNumbersToLink.addAll(IntStream.range(lowBound, highBound)
+				.mapToObj(x -> x)
+				.collect(Collectors.toList()));
+		
+		// add last page
+		pageNumbersToLink.add(numberOfPages);
+		
+		return pageNumbersToLink;
+	}
+	
+	// helper function
+	// efficient way to compute ceil(x/2)
+	private int ceil2(int x) {
+		return (x / 2) + (x % 2);
+	}
+	
+	/**
+	 * Get the entities on a page
+	 * @param pageNumber the page number
+	 * @return the list of entities on the page
+	 */
+	public List<E> getPage(int pageNumber) {
+		int firstElementIndex = firstElementIndices.get(pageNumber);
+		return entityList.subList(firstElementIndex,
+				Math.min(entityList.size(), firstElementIndex + entitiesPerPage));
+	}
+
+	/**
+	 * Get the maximum number of consecutive pages around the current page
+	 * to which to display links
+	 * @return the maximum number of consecutive pages
+	 */
+	public int getNumberOfConsecutivePagesToLink() {
+		return numberOfConsecutivePagesToLink;
+	}
+
+	/**
+	 * Set the maximum number of consecutive pages around the current page
+	 * to which to display links
+	 * @return the maximum number of consecutive pages
+	 */
+	public void setNumberOfConsecutivePagesToLink(int numberOfConsecutivePagesToLink) {
+		this.numberOfConsecutivePagesToLink = numberOfConsecutivePagesToLink;
+	}
+}
