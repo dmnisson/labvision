7e48f716c6523411bb95ba5111c9215f506053a2
David Nisson <d.m.nisson@gmail.com>
2019-12-31 14:02:28 -0800

    display names for quantity types

--- src/main/java/labvision/entities/QuantityTypeId.java
+++ src/main/java/labvision/entities/QuantityTypeId.java
@@ -13,45 +13,46 @@ import labvision.measure.SI;
  *
  */
 public enum QuantityTypeId {
-	UNKNOWN(null),
-	DIMENSIONLESS(new QuantityClass<>(Dimensionless.class)),
-	ANGLE(new QuantityClass<>(Angle.class)),
-	SOLID_ANGLE(new QuantityClass<>(SolidAngle.class)),
-	LENGTH(new QuantityClass<>(Length.class)),
-	AREA(new QuantityClass<>(Area.class)),
-	VOLUME(new QuantityClass<>(Volume.class)),
-	MASS(new QuantityClass<>(Mass.class)),
-	TIME(new QuantityClass<>(Time.class)),
-	SPEED(new QuantityClass<>(Speed.class)),
-	ACCELERATION(new QuantityClass<>(Acceleration.class)),
-	FORCE(new QuantityClass<>(Force.class)),
-	ENERGY(new QuantityClass<>(Energy.class)),
-	POWER(new QuantityClass<>(Power.class)),
-	PRESSURE(new QuantityClass<>(Pressure.class)),
-	ELECTRIC_CHARGE(new QuantityClass<>(ElectricCharge.class)),
-	ELECTRIC_CURRENT(new QuantityClass<>(ElectricCurrent.class)),
-	ELECTRIC_POTENTIAL(new QuantityClass<>(ElectricPotential.class)),
-	ELECTRIC_RESISTANCE(new QuantityClass<>(ElectricResistance.class)),
-	ELECTRIC_CONDUCTANCE(new QuantityClass<>(ElectricConductance.class)),
-	ELECTRIC_CAPACITANCE(new QuantityClass<>(ElectricCapacitance.class)),
-	ELECTRIC_INDUCTANCE(new QuantityClass<>(ElectricInductance.class)),
-	TEMPERATURE(new QuantityClass<>(Temperature.class)),
-	AMOUNT_OF_SUBSTANCE(new QuantityClass<>(AmountOfSubstance.class)),
-	CATALYTIC_ACTIVITY(new QuantityClass<>(CatalyticActivity.class)),
-	LUMINOUS_FLUX(new QuantityClass<>(LuminousFlux.class)),
-	LUMINOUS_INTENSITY(new QuantityClass<>(LuminousIntensity.class)),
-	ILLUMINANCE(new QuantityClass<>(Illuminance.class)),
-	MAGNETIC_FLUX(new QuantityClass<>(MagneticFlux.class)),
-	MAGNETIC_FLUX_DENSITY(new QuantityClass<>(MagneticFluxDensity.class)),
-	RADIATION_DOSE_ABSORBED(new QuantityClass<>(RadiationDoseAbsorbed.class)),
-	RADIATION_DOSE_EFFECTIVE(new QuantityClass<>(RadiationDoseEffective.class)),
-	RADIOACTIVITY(new QuantityClass<>(Radioactivity.class))
-	;
+	UNKNOWN(null, "Custom"),
+	DIMENSIONLESS(new QuantityClass<>(Dimensionless.class), "Dimensionless"),
+	ANGLE(new QuantityClass<>(Angle.class), "Angle"),
+	SOLID_ANGLE(new QuantityClass<>(SolidAngle.class), "Solid Angle"),
+	LENGTH(new QuantityClass<>(Length.class), "Length"),
+	AREA(new QuantityClass<>(Area.class), "Area"),
+	VOLUME(new QuantityClass<>(Volume.class), "Volume"),
+	MASS(new QuantityClass<>(Mass.class), "Mass"),
+	TIME(new QuantityClass<>(Time.class), "Time"),
+	SPEED(new QuantityClass<>(Speed.class), "Speed"),
+	ACCELERATION(new QuantityClass<>(Acceleration.class), "Acceleration"),
+	FORCE(new QuantityClass<>(Force.class), "Force"),
+	ENERGY(new QuantityClass<>(Energy.class), "Energy"),
+	POWER(new QuantityClass<>(Power.class), "Power"),
+	PRESSURE(new QuantityClass<>(Pressure.class), "Pressure"),
+	ELECTRIC_CHARGE(new QuantityClass<>(ElectricCharge.class), "Electric Charge"),
+	ELECTRIC_CURRENT(new QuantityClass<>(ElectricCurrent.class), "Electric Current"),
+	ELECTRIC_POTENTIAL(new QuantityClass<>(ElectricPotential.class), "Electric Potential"),
+	ELECTRIC_RESISTANCE(new QuantityClass<>(ElectricResistance.class), "Electric Resistance"),
+	ELECTRIC_CONDUCTANCE(new QuantityClass<>(ElectricConductance.class), "Electric Conductance"),
+	ELECTRIC_CAPACITANCE(new QuantityClass<>(ElectricCapacitance.class), "Electric Capacitance"),
+	ELECTRIC_INDUCTANCE(new QuantityClass<>(ElectricInductance.class), "Electric Inductance"),
+	TEMPERATURE(new QuantityClass<>(Temperature.class), "Temperature"),
+	AMOUNT_OF_SUBSTANCE(new QuantityClass<>(AmountOfSubstance.class), "Amount of a Substance"),
+	CATALYTIC_ACTIVITY(new QuantityClass<>(CatalyticActivity.class), "Catalytic Activity"),
+	LUMINOUS_FLUX(new QuantityClass<>(LuminousFlux.class), "Luminous Flux"),
+	LUMINOUS_INTENSITY(new QuantityClass<>(LuminousIntensity.class), "Luminous Intensity"),
+	ILLUMINANCE(new QuantityClass<>(Illuminance.class), "Illuminance"),
+	MAGNETIC_FLUX(new QuantityClass<>(MagneticFlux.class), "Magnetic Flux"),
+	MAGNETIC_FLUX_DENSITY(new QuantityClass<>(MagneticFluxDensity.class), "Magnetic Flux Density"),
+	RADIATION_DOSE_ABSORBED(new QuantityClass<>(RadiationDoseAbsorbed.class), "Absorbed Radiation Dose"),
+	RADIATION_DOSE_EFFECTIVE(new QuantityClass<>(RadiationDoseEffective.class), "Effective Radiation Dose"),
+	RADIOACTIVITY(new QuantityClass<>(Radioactivity.class), "Radioactivity");
 	
 	private final QuantityClass<?> quantityClass;
-
-	private QuantityTypeId(QuantityClass<?> quantityClass) {
+	private final String displayName;
+		
+	private QuantityTypeId(QuantityClass<?> quantityClass, String displayName) {
 		this.quantityClass = quantityClass;
+		this.displayName = displayName;
 	}
 	
 	public QuantityClass<?> getQuantityClass() {
@@ -75,5 +76,9 @@ public enum QuantityTypeId {
 					qt.quantityClass.getQuantityType().equals(quantityType))
 				.findFirst()
 				.orElse(UNKNOWN);
+	}
+	
+	public String getDisplayName() {
+		return displayName;
 	}
 }
