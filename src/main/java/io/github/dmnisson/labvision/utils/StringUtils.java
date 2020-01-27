46b6ae69b270f2902e95bdee9bdce313e928cd7e
David Nisson <d.m.nisson@gmail.com>
2020-01-26 18:25:46 -0800

    remove old StringUtils class (Spring already has one)

--- src/main/java/io/github/dmnisson/labvision/utils/StringUtils.java
+++
@@ -1,9 +0,0 @@
-package io.github.dmnisson.labvision.utils;
-
-public class StringUtils {
-
-	public static boolean isNullOrEmpty(String string) {
-		return string == null || string.isEmpty();
-	}
-
-}
