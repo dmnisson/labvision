25ea25f4626dfb53f22c4cbcabd4ea25291b113b
David Nisson <d.m.nisson@gmail.com>
2020-02-17 17:53:20 -0800

    access control for report documents

--- src/main/java/io/github/dmnisson/labvision/reportdocs/ReportDocumentController.java
+++ src/main/java/io/github/dmnisson/labvision/reportdocs/ReportDocumentController.java
@@ -16,9 +16,13 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 
+import io.github.dmnisson.labvision.AccessDeniedException;
 import io.github.dmnisson.labvision.ResourceNotFoundException;
+import io.github.dmnisson.labvision.entities.Experiment;
 import io.github.dmnisson.labvision.entities.FilesystemReportDocument;
 import io.github.dmnisson.labvision.entities.LabVisionUser;
+import io.github.dmnisson.labvision.repositories.CourseRepository;
+import io.github.dmnisson.labvision.repositories.ExperimentRepository;
 import io.github.dmnisson.labvision.repositories.FilesystemReportDocumentRepository;
 
 @Controller
@@ -29,18 +33,45 @@ public class ReportDocumentController {
 	private FilesystemReportDocumentRepository filesystemReportDocumentRepository;
 	
 	@Autowired
+	private ExperimentRepository experimentRepository;
+	
+	@Autowired
+	private CourseRepository courseRepository;
+	
+	@Autowired
 	private ReportDocumentService reportDocumentService;
 	
 	@GetMapping("/{studentId}/{experimentId}/{filename}")
 	public void getReportDocument(
 			@PathVariable Integer studentId, @PathVariable Integer experimentId, @PathVariable String filename, 
-			@AuthenticationPrincipal(expression="labVisionUser") LabVisionUser user, HttpServletResponse response) throws IOException {
+			@AuthenticationPrincipal(expression="labVisionUser") LabVisionUser user, HttpServletResponse response) throws IOException, AccessDeniedException {
 		
 		Path filesystemPath = reportDocumentService.buildFilesystemPath(experimentId, studentId, filename);
 		
 		FilesystemReportDocument filesystemReportDocument = 
 				filesystemReportDocumentRepository.findByFilesystemPath(filesystemPath.normalize().toString())
 				.orElseThrow(() -> new ResourceNotFoundException(FilesystemReportDocument.class, filename));
+		
+		// check if the user is either the student who submitted the report or an instructor who owns or
+		// teaches the course for the experiment
+		boolean authorized;
+		switch (user.getRole()) {
+		case STUDENT:
+			authorized = filesystemReportDocument.getReportedResult().getStudent().getId().equals(user.getId());
+			break;
+		case FACULTY:
+			Experiment experiment = experimentRepository.getOne(
+					filesystemReportDocument.getReportedResult().getExperiment().getId());
+			authorized = experimentRepository.findByIdForInstructor(experiment.getId(), user.getId()).isPresent()
+					|| courseRepository.findByIdForInstructor(experiment.getCourse().getId(), user.getId()).isPresent();
+			break;
+		default:
+			authorized = false;
+		}
+		
+		if (!authorized) {
+			throw new AccessDeniedException(FilesystemReportDocument.class, filesystemReportDocument.getId());
+		}
 		
 		File file = filesystemReportDocument.getReportDocumentFile();
 		
