8bb9dd2063b2917ff31f5ffd7bd9fbc0045f1046
David Nisson <d.m.nisson@gmail.com>
2020-02-19 18:54:30 -0800

    add token-based password reset functionality

--- src/main/java/io/github/dmnisson/labvision/ResetPasswordController.java
+++ src/main/java/io/github/dmnisson/labvision/ResetPasswordController.java
@@ -1,5 +1,7 @@
 package io.github.dmnisson.labvision;
 
+import java.util.Objects;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.core.AuthenticationException;
@@ -9,6 +11,7 @@ import org.springframework.security.crypto.password.PasswordEncoder;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.servlet.mvc.method.annotation.MvcUriComponentsBuilder;
@@ -43,25 +46,26 @@ public class ResetPasswordController {
 		return "resetpassword";
 	}
 	
+	@GetMapping("/begin/{token}")
+	public String beginPasswordResetWithToken(@PathVariable String token, Model model) {
+		model.addAttribute("actionUrl", MvcUriComponentsBuilder
+				.fromMethodName(ResetPasswordController.class, "updatePasswordWithToken",
+						token, null, null)
+				.replaceQuery(null)
+				.build()
+				.toUriString());
+		
+		model.addAttribute("token", token);
+		
+		return "resetpassword";
+	}
+	
 	@PostMapping("/set")
 	public String updatePassword(
 			String username, String password, String newPassword, String confirmNewPassword,
 			@AuthenticationPrincipal UserDetails user) {
-		String error = null;
-		
-		if (!user.getUsername().equalsIgnoreCase(username)) {
-			error = "unauthorized";
-		} else if (!newPassword.equals(confirmNewPassword)) {
-			error = "unmatched";
-		} else {
-			try {
-				userDetailsManager.setAuthenticationManager(authenticationManager);
-				userDetailsManager.loadUserByUsername(username);
-				userDetailsManager.changePassword(password, passwordEncoder.encode(newPassword));
-			} catch (AuthenticationException ex) {
-				error = "unauthorized";
-			}
-		}
+		String error = changePasswordInternal(username, password, newPassword, confirmNewPassword, user,
+				authenticationManager, null);
 		
 		if (error != null) {
 			return "redirect:" + MvcUriComponentsBuilder
@@ -71,5 +75,50 @@ public class ResetPasswordController {
 		}
 		
 		return "redirect:" + dashboardUrlService.getDashboardUrl(user.getAuthorities());
+	}
+	
+	@PostMapping("/set/{token}")
+	public String updatePasswordWithToken(
+			@PathVariable String token, String newPassword, String confirmNewPassword) {
+		UserDetails user = userDetailsManager.loadUserByPasswordResetToken(token);
+		
+		String username = user.getUsername();
+		String error = changePasswordInternal(username, "temp_" + username, newPassword, confirmNewPassword,
+				user, null, token);
+		
+		if (error != null) {
+			return "redirect:" + MvcUriComponentsBuilder
+					.fromMethodName(ResetPasswordController.class, "beginPasswordResetWithToken", token, null)
+					.replaceQueryParam("error", error)
+					.build().toUriString();
+		}
+		
+		userDetailsManager.clearPasswordResetToken(user.getUsername());
+		
+		return "redirect:" + dashboardUrlService.getDashboardUrl(user.getAuthorities());
+	}
+	
+	private String changePasswordInternal(String username, String password, String newPassword, String confirmNewPassword,
+			UserDetails user, final AuthenticationManager authenticationManagerToSet, String token) {
+		String error = null;
+		
+		if (!user.getUsername().equalsIgnoreCase(username)) {
+			error = "unauthorized";
+		} else if (!newPassword.equals(confirmNewPassword)) {
+			error = "unmatched";
+		} else {
+			try {
+				userDetailsManager.setAuthenticationManager(authenticationManagerToSet);
+				userDetailsManager.loadUserByUsername(username);
+				if (Objects.isNull(token)) {
+					userDetailsManager.changePassword(password, passwordEncoder.encode(newPassword));
+				} else {
+					userDetailsManager.changePasswordWithToken(token, passwordEncoder.encode(newPassword));
+				}
+			} catch (AuthenticationException ex) {
+				error = "unauthorized";
+			}
+		}
+		return error;
 	}
 }
