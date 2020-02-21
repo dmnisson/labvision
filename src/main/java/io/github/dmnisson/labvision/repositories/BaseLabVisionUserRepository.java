405674a6d44c3675adcbe9f1ca8272f947d05a13
David Nisson <d.m.nisson@gmail.com>
2020-02-20 20:34:38 -0800

    password reset token functionality

--- src/main/java/io/github/dmnisson/labvision/repositories/BaseLabVisionUserRepository.java
+++ src/main/java/io/github/dmnisson/labvision/repositories/BaseLabVisionUserRepository.java
@@ -4,10 +4,16 @@ import java.util.Optional;
 
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.data.repository.NoRepositoryBean;
+import org.springframework.transaction.annotation.Transactional;
 
 import io.github.dmnisson.labvision.entities.LabVisionUser;
 
 @NoRepositoryBean
 public interface BaseLabVisionUserRepository<U extends LabVisionUser> extends CrudRepository<U, Integer> {
 	Optional<U> findByUsername(String username);
+	
+	Optional<U> findByPasswordResetToken(String token);
+	
+	@Transactional
+	void deleteByUsername(String username);
 }
