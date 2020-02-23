60d61121db11442e65719ee02cde50e4f07fd746
David Nisson <d.m.nisson@gmail.com>
2020-02-22 19:27:42 -0800

    bring back static files

--- src/main/java/io/github/dmnisson/labvision/LabVisionWebMvcConfig.java
+++ src/main/java/io/github/dmnisson/labvision/LabVisionWebMvcConfig.java
@@ -6,6 +6,7 @@ import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.method.support.HandlerMethodArgumentResolver;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 import io.github.dmnisson.labvision.pagination.MapPageableHandlerMethodArgumentResolver;
@@ -13,7 +14,7 @@ import io.github.dmnisson.labvision.pagination.MapPageableHandlerMethodArgumentR
 @Configuration
 @EnableWebMvc
 public class LabVisionWebMvcConfig implements WebMvcConfigurer {
-
+	
 	@Override
 	public void addArgumentResolvers(List<HandlerMethodArgumentResolver> resolvers) {
 		resolvers.add(mapPageableResolver());
@@ -22,6 +23,13 @@ public class LabVisionWebMvcConfig implements WebMvcConfigurer {
 	@Bean
 	public MapPageableHandlerMethodArgumentResolver mapPageableResolver() {
 		return new MapPageableHandlerMethodArgumentResolver();
+	}
+
+	@Override
+	public void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("/**")
+			.addResourceLocations("classpath:/static/")
+			.setCachePeriod(31558150);
 	}
 
 }
