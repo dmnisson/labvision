c1ac5a5f106e58b1b2cb12aae711a04ebc21adb5
David Nisson <d.m.nisson@gmail.com>
2020-03-03 22:40:21 -0800

    add method in service layer to update student preferences

--- src/main/java/io/github/dmnisson/labvision/student/StudentPreferencesService.java
+++ src/main/java/io/github/dmnisson/labvision/student/StudentPreferencesService.java
@@ -1,10 +1,14 @@
 package io.github.dmnisson.labvision.student;
 
+import java.util.Optional;
+
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import io.github.dmnisson.labvision.entities.Student;
 import io.github.dmnisson.labvision.entities.StudentPreferences;
 import io.github.dmnisson.labvision.repositories.StudentPreferencesRepository;
+import io.github.dmnisson.labvision.repositories.StudentRepository;
 
 @Service
 public class StudentPreferencesService {
@@ -14,6 +18,9 @@ public class StudentPreferencesService {
 
 	@Autowired
 	private StudentPreferencesRepository studentPreferencesRepository;
+
+	@Autowired
+	private StudentRepository studentRepository;
 	
 	public int getMaxCurrentExperiments(Integer studentId) {
 		return studentPreferencesRepository.findMaxCurrentExperimentsByStudentId(studentId)
@@ -48,6 +55,30 @@ public class StudentPreferencesService {
 				);
 		
 		return preferences;
+	}
+	
+	public void updateStudentPreferences(Integer studentId, StudentPreferences newStudentPreferences) {
+		
+		Student student = studentRepository.findById(studentId).get();
+		
+		Optional<StudentPreferences> oldStudentPreferences = studentPreferencesRepository.findByStudentId(studentId);
+		
+		if (!oldStudentPreferences.isPresent()) {
+			student.setStudentPreferences(newStudentPreferences);
+		} else {
+			StudentPreferences studentPreferences = oldStudentPreferences.get();
+			studentPreferences.setMaxCurrentExperiments(
+					newStudentPreferences.getMaxCurrentExperiments()
+					);
+			studentPreferences.setMaxRecentExperiments(
+					newStudentPreferences.getMaxRecentExperiments()
+					);
+			studentPreferences.setMaxRecentCourses(
+					newStudentPreferences.getMaxRecentCourses()
+					);
+		}
+		
+		studentRepository.save(student);
 	}
 	
 }
--- src/test/java/io/github/dmnisson/labvision/TestStudentPreferencesService.java
+++ src/test/java/io/github/dmnisson/labvision/TestStudentPreferencesService.java
@@ -1,21 +1,26 @@
 package io.github.dmnisson.labvision;
 
-import org.junit.jupiter.api.Test;
-import org.springframework.beans.factory.annotation.Autowired;
-import org.springframework.boot.test.mock.mockito.MockBean;
-
-import io.github.dmnisson.labvision.entities.StudentPreferences;
-import io.github.dmnisson.labvision.repositories.StudentPreferencesRepository;
-import io.github.dmnisson.labvision.student.StudentPreferencesConfig;
-import io.github.dmnisson.labvision.student.StudentPreferencesService;
-
 import static org.junit.jupiter.api.Assertions.assertEquals;
 import static org.junit.jupiter.api.Assertions.assertNotNull;
 import static org.junit.jupiter.api.Assertions.assertNull;
 import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.spy;
+import static org.mockito.Mockito.times;
+import static org.mockito.Mockito.verify;
 import static org.mockito.Mockito.when;
 
 import java.util.Optional;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.mock.mockito.MockBean;
+
+import io.github.dmnisson.labvision.entities.Student;
+import io.github.dmnisson.labvision.entities.StudentPreferences;
+import io.github.dmnisson.labvision.repositories.StudentPreferencesRepository;
+import io.github.dmnisson.labvision.repositories.StudentRepository;
+import io.github.dmnisson.labvision.student.StudentPreferencesConfig;
+import io.github.dmnisson.labvision.student.StudentPreferencesService;
 
 public class TestStudentPreferencesService extends LabvisionApplicationTests {
 
@@ -24,6 +29,9 @@ public class TestStudentPreferencesService extends LabvisionApplicationTests {
 	
 	@MockBean
 	private StudentPreferencesRepository studentPreferencesRepository;
+	
+	@MockBean
+	private StudentRepository studentRepository;
 	
 	@Autowired
 	private StudentPreferencesService studentPreferencesService;
@@ -167,5 +175,66 @@ public class TestStudentPreferencesService extends LabvisionApplicationTests {
 		assertEquals(maxCurrentExperiments, actualDefaultStudentPreferences.getMaxCurrentExperiments());
 		assertEquals(maxRecentExperiments, actualDefaultStudentPreferences.getMaxRecentExperiments());
 		assertEquals(maxRecentCourses, actualDefaultStudentPreferences.getMaxRecentCourses());
+	}
+	
+	@Test
+	public void updateStudentPreferences_ShouldCreateNewStudentPreferencesIfPreviouslyNull() {
+		final Integer studentId = 6;
+		
+		Student student = new Student();
+		student.setId(studentId);
+		
+		Student spyStudent = spy(student);
+		
+		when(studentRepository.findById(eq(studentId)))
+			.thenReturn(Optional.of(spyStudent));
+		
+		when(studentPreferencesRepository.findByStudentId(eq(studentId)))
+			.thenReturn(Optional.empty());
+		
+		StudentPreferences newStudentPreferences = new StudentPreferences();
+		newStudentPreferences.setMaxCurrentExperiments(8);
+		
+		studentPreferencesService.updateStudentPreferences(studentId, newStudentPreferences);
+		
+		verify(spyStudent, times(1)).setStudentPreferences(newStudentPreferences);
+		verify(studentRepository, times(1)).save(spyStudent);
+	}
+	
+	@Test
+	public void updateStudentPreferences_ShouldUpdateStudentPreferences() {
+		final int studentId = 6;
+		
+		Student student = new Student();
+		student.setId(studentId);
+		
+		StudentPreferences studentPreferences = new StudentPreferences();
+		studentPreferences.setId(7);
+		studentPreferences.setMaxCurrentExperiments(6);
+		
+		StudentPreferences spyStudentPreferences = spy(studentPreferences);
+		
+		StudentPreferences newStudentPreferences = new StudentPreferences();
+		newStudentPreferences.setMaxCurrentExperiments(8);
+		newStudentPreferences.setMaxRecentExperiments(4);
+		
+		when(studentRepository.findById(eq(studentId)))
+			.thenReturn(Optional.of(student));
+		
+		when(studentPreferencesRepository.findByStudentId(eq(studentId)))
+			.thenReturn(Optional.of(spyStudentPreferences));
+		
+		studentPreferencesService.updateStudentPreferences(studentId, newStudentPreferences);
+		
+		verify(spyStudentPreferences, times(1))
+			.setMaxCurrentExperiments(newStudentPreferences.getMaxCurrentExperiments());
+		
+		verify(spyStudentPreferences, times(1))
+			.setMaxRecentExperiments(newStudentPreferences.getMaxRecentExperiments());
+		
+		verify(spyStudentPreferences, times(1))
+			.setMaxRecentCourses(newStudentPreferences.getMaxRecentCourses());
+		
+		verify(studentRepository, times(1)).save(student);
 	}
 }
